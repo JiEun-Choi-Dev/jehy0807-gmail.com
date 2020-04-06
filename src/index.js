1a672405fbdf37fe4884a037c40fde17b23e295b
jieunchoi <jehy0807@gmail.com>
2020-04-06 10:56:15 +0900

    Add project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+import { createStore } from "redux";
+import reducers from "./reducers";
+// import * as actions from "./actions";
+import { Provider } from "react-redux";
+
+const store = createStore(reducers);
+console.log(store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+// store.subscribe(() => console.log(store.getState()));
+// store.dispatch(actions.increment());
+// store.dispatch(actions.increment());
+// store.dispatch(actions.decrement());
+// store.dispatch(actions.setColor([200, 200, 200]));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// unsubscribe();
